2e41410286d98e0048f54f4ec11acba0df97e250
Mohammed Ayowa <mohammed.ayowa@scania.com>
2016-03-23 16:32:52 +0100

    device service

--- src/scania-angular-ui.js
+++ src/scania-angular-ui.js
@@ -11,6 +11,8 @@
 
     angular.module('scania.angular.ui', ['scania.angular.lightbox', 'scania.angular.select2']);
 
+    angular.module('scania.angular.ui').service('device', ['$rootScope', '$window', device]);
+
     /**
      * @ngdoc module
      * @name scania.angular.lightbox
@@ -18,7 +20,7 @@
      * @description
      * Scania lightbox module
      */
-    angular.module('scania.angular.lightbox', ['flow', 'ui.bootstrap']).directive('scLightbox', ['$scmodal', scLightbox]);
+    angular.module('scania.angular.lightbox', ['flow', 'ui.bootstrap']).directive('scLightbox', ['$scmodal', '$timeout', '$q', '$window', scLightbox]);
     /**
      /**
      * @ngdoc directive
@@ -26,25 +28,46 @@
      * @module scania.angular.lightbox
      *
      * @description file upload and gallery extension on angularJs framework
-     * @param $scmodal
+     * @param $scmodal a custom $modal service to avoid conflict angularStrap and ui-bootstrap.
      * @returns {{restrict: string, templateUrl: string, controllerAs: string, controller: Function}}
      */
 
-    function scLightbox($scmodal) {
+    function scLightbox($scmodal, $timeout, $q, $window) {
 
         return {
             restrict: 'AEC',
-            templateUrl: 'template/scania-angular-upload.html',
+            template: '<ng-include src="getTemplateUrl()" />',
             controllerAs: 'lightbox',
-            controller: function ($scope, $attrs) {
-                $animate.enabled(false); // to solve the conflict between ngAnimate and ui-bootstrap animate
+            scope: {
+                ngModel: '='
+            },
+            controller: function ($scope, $attrs, flowFactory) {
                 var self = this;
+                self.files = []
+
+                $timeout(function(){
+                    $scope.$watch('ngModel', function(){
+                        if(!$scope.ngModel) return;
+                        self.defaultImageSrcs = $scope.ngModel;
+                        $scope.flowObj = flowFactory.create();
+                        _.each($scope.ngModel, function(imgSrc){
+                            convertToBlob(imgSrc).then(function(file){
+                                file.src = imgSrc;
+                                $scope.flowObj.addFile(file);
+                            });
+                        });
+                    });
+                });
 
                 self.interval = $attrs.interval;
                 self.open = function (index) {
-                    $scope.slides = self.flow.files;
+                    $scope.slides = ($scope.flowObj) ? $scope.flowObj.files :  self.flow.files;
                     $scope.slides[index].active = true;
-                    self.activeImage = $scope.slides[index];
+                    if(!$scope.ngModel){
+                        _.each($scope.slides, function(slide){
+                            slide.file.src = $window.URL.createObjectURL(slide.file);
+                        });
+                    }
 
                     self.modalInstance = $scmodal.open({
                         animation: $attrs.animation,
@@ -56,21 +79,58 @@
                             slides: function () {
                                 return $scope.slides;
                             }
-                        },
-                        controller: function ($scope) {
-                            $scope.setAsActive = function (slide) {
-                                if (slide.active) {
-                                    self.activeImage = slide;
-                                }
-                            };
                         }
                     });
                 };
                 self.deleteImage = function () {
-                    self.activeImage.cancel();
+                    $scope.slides.splice(_.findIndex($scope.slides, function(slide){
+                        if(slide.active) {
+                            var nextSlide = $scope.slides[$scope.slides.indexOf(slide) + 1] ? $scope.slides[$scope.slides.indexOf(slide) + 1] : $scope.slides[0];
+                            if(nextSlide) nextSlide.active = true;
+                            slide.active = false;
+
+                            return true;
+                        };
+                    }), 1);
                     if ($scope.slides.length === 0) {
-                        self.modalInstance.dismiss();
+                        self.modalInstance.dismiss('cancel');
                     }
+
+                };
+                self.close = function(){
+                    self.modalInstance.dismiss('cancel');
+                }
+
+                function convertToBlob(url){
+                    var deferred = $q.defer(), img = new Image();
+                    img.crossOrigin = '';
+                    img.onload = function(){
+                        var canvas = document.createElement('CANVAS');
+                        var ctx = canvas.getContext('2d');
+                        var dataURL;
+                        canvas.height = this.height;
+                        canvas.width = this.width;
+                        ctx.drawImage(this, 0, 0);
+                        dataURL = canvas.toDataURL('image/png');
+                        var byteString = dataURL.split(',')[1];
+                        var mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
+
+                        var byteBuffer = new ArrayBuffer(byteString.length);
+                        var ia = new Uint8Array(byteBuffer);
+                        for (var i = 0; i < byteString.length; i++) {
+                            ia[i] = byteString.charCodeAt(i);
+                        }
+
+                        var file = new File([byteBuffer], 'image-' + Math.floor((Math.random() * 1000) + 1) + '.' + mimeString.split('/')[1],  {type: mimeString, lastModified: new Date()});
+                        deferred.resolve(file);
+                        canvas = null;
+                    };
+                    img.src = url;
+                    return deferred.promise;
+                }
+
+                $scope.getTemplateUrl = function () {
+                    return ($scope.ngModel) ? 'template/scania-angular-preview.html' : 'template/scania-angular-upload.html';
                 };
             }
         };
@@ -162,7 +222,6 @@
                         populatePreselectedOptions(select, selectedItems, options.value);
                     }
                 }
-
                 function registerSearchInputEvents() {
                     $('.select2-input').bind(events, function (event) {
                         var minimumInputLength = (options.minimumInputLength) ? options.minimumInputLength : 3;
@@ -199,4 +258,79 @@
         }
     }
 
+    /**
+     * @ngdoc service
+     * @name device
+     * @module scania.bootstrap.ui
+     *
+     * @description
+     * Provide a service for detector if the device is a mobile or desktop
+     *
+     * @returns {{service: Object}}
+     *
+     */
+
+    function device($rootScope, $window) {
+
+        $rootScope.$watch(function() {
+            return $window.innerWidth;
+        }, function(){
+            _emitDetectedDevice();
+        });
+
+        var  _events = {
+            device: 'device.detected',
+            largedevice: 'large.device.detected',
+            phone: 'phone.detected',
+            portraittablet: 'portrait.tablet.detected',
+            landscapetablet: 'landscape.tablet.detected',
+            desktop: 'desktop.detected'
+        }
+        var service = {
+            isDevice : _isDevice,
+            isLargeDevice : _isLargeDevice,
+            events: _events
+        }
+
+        return service;
+
+        function _isDevice() {
+            return _isSmallPhone() || _isMediumDevice() || _isPortraitTablet();
+        }
+        function _isLargeDevice() {
+            return  _isLandscapeTablet() || _isDesktop() || _isLargeDesktop();
+        }
+        function _emitDetectedDevice() {
+            //4 break points including phones (portrait and landscape), tablets (portrait or landscape), laptops and desktop
+            $rootScope.$emit(_events.phone, _isSmallPhone() || _isMediumDevice());
+            $rootScope.$emit(_events.portraittablet, _isPortraitTablet());
+            $rootScope.$emit(_events.landscapetablet, _isLandscapeTablet());
+            $rootScope.$emit(_events.desktop, _isDesktop() || _isLargeDesktop());
+
+            //1 break point between mobile/tablet and desktop
+            $rootScope.$emit(_events.device, _isDevice());
+            $rootScope.$emit(_events.largedevice,_isLargeDevice());
+
+        }
+
+        function _isSmallPhone() {
+            return window.matchMedia("only screen and (max-width: 480px)").matches;
+        }
+        //Landscape phone and portrait small tablets
+        function _isMediumDevice() {
+            return window.matchMedia("only screen and (min-width: 481px) and (max-width: 767px)").matches;
+        }
+        function _isPortraitTablet() {
+            return window.matchMedia("only screen and (min-width: 768px) and (max-width: 1024px) and (orientation: portrait)").matches;
+        }
+        function _isLandscapeTablet() {
+            return window.matchMedia("only screen and (min-width: 768px) and (max-width: 1024px) and (orientation: landscape)").matches;
+        }
+        function _isDesktop() {
+            return window.matchMedia("only screen and (min-width: 1024px) and (max-width: 1199px)").matches;
+        }
+        function _isLargeDesktop() {
+            return window.matchMedia("only screen and (min-width: 1200px)").matches;
+        }
+    }
 })();
